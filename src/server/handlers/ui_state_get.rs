c9128db7ab89622d917379450ea44aa41e981856
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2023-01-09 22:05:50 +0100

    [Fix] Remove not-yet-implemented utils that are not planned for MVP from the utils list.

--- src/server/handlers/ui_state_get.rs
+++ src/server/handlers/ui_state_get.rs
@@ -60,107 +60,50 @@ pub async fn ui_state_get(
                 id: "webhooks".to_string(),
                 name: "Webhooks".to_string(),
                 icon: Some("node".to_string()),
-                utils: Some(vec![
-                    Util {
-                        id: "webhooks__responders".to_string(),
-                        name: "Responders".to_string(),
-                        icon: None,
-                        utils: None,
-                    },
-                    Util {
-                        id: "webhooks__triggers".to_string(),
-                        name: "Triggers".to_string(),
-                        icon: None,
-                        utils: None,
-                    },
-                ]),
+                utils: Some(vec![Util {
+                    id: "webhooks__responders".to_string(),
+                    name: "Responders".to_string(),
+                    icon: None,
+                    utils: None,
+                }]),
             },
             Util {
                 id: "certificates".to_string(),
                 name: "Digital Certificates".to_string(),
                 icon: Some("securityApp".to_string()),
-                utils: Some(vec![
-                    Util {
-                        id: "certificates__root".to_string(),
-                        name: "Root certificates".to_string(),
-                        icon: None,
-                        utils: None,
-                    },
-                    Util {
-                        id: "certificates__leaf".to_string(),
-                        name: "Leaf certificates".to_string(),
-                        icon: None,
-                        utils: None,
-                    },
-                    Util {
-                        id: "certificates__explorer".to_string(),
-                        name: "Certificates explorer".to_string(),
-                        icon: None,
-                        utils: None,
-                    },
-                ]),
+                utils: Some(vec![Util {
+                    id: "certificates__root".to_string(),
+                    name: "Root certificates".to_string(),
+                    icon: None,
+                    utils: None,
+                }]),
             },
             Util {
                 id: "web_security".to_string(),
                 name: "Web Security".to_string(),
                 icon: Some("globe".to_string()),
-                utils: Some(vec![
-                    Util {
-                        id: "web_security__csp".to_string(),
-                        name: "CSP".to_string(),
+                utils: Some(vec![Util {
+                    id: "web_security__csp".to_string(),
+                    name: "CSP".to_string(),
+                    icon: None,
+                    utils: Some(vec![Util {
+                        id: "web_security__csp__policies".to_string(),
+                        name: "Policies".to_string(),
                         icon: None,
-                        utils: Some(vec![
-                            Util {
-                                id: "web_security__csp__policies".to_string(),
-                                name: "Policies".to_string(),
-                                icon: None,
-                                utils: None,
-                            },
-                            Util {
-                                id: "web_security__csp__explorer".to_string(),
-                                name: "Policies explorer".to_string(),
-                                icon: None,
-                                utils: None,
-                            },
-                        ]),
-                    },
-                    Util {
-                        id: "web_security__cors".to_string(),
-                        name: "CORS".to_string(),
-                        icon: None,
-                        utils: Some(vec![Util {
-                            id: "web_security__cors__explorer".to_string(),
-                            name: "Policies explorer".to_string(),
-                            icon: None,
-                            utils: None,
-                        }]),
-                    },
-                ]),
+                        utils: None,
+                    }]),
+                }]),
             },
             Util {
                 id: "web_scrapping".to_string(),
                 name: "Web Scrapping".to_string(),
                 icon: Some("cut".to_string()),
-                utils: Some(vec![
-                    Util {
-                        id: "web_scrapping__screenshots".to_string(),
-                        name: "Screenshots".to_string(),
-                        icon: None,
-                        utils: None,
-                    },
-                    Util {
-                        id: "web_scrapping__resources".to_string(),
-                        name: "Resources scrapper".to_string(),
-                        icon: None,
-                        utils: None,
-                    },
-                    Util {
-                        id: "web_scrapping__tables".to_string(),
-                        name: "Tables scrapper".to_string(),
-                        icon: None,
-                        utils: None,
-                    },
-                ]),
+                utils: Some(vec![Util {
+                    id: "web_scrapping__resources".to_string(),
+                    name: "Resources scrapper".to_string(),
+                    icon: None,
+                    utils: None,
+                }]),
             },
         ],
     }))
