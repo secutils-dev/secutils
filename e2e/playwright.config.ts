a4c28cb4e7f849d25e1e8dd57738cbeafc636669
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2026-02-19 11:59:22 +0100

    chore(ci, e2e): capture Playwright reports on CI

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? 'github' : 'html',
+  reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'html',
   use: {
     baseURL: process.env.BASE_URL ?? 'http://localhost:7171',
     trace: 'on-first-retry',
